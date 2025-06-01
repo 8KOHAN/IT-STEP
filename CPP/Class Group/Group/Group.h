a0c7229e9235d134af558071ce4bd6cb80744763
Alexey Kohan <kohanzom@gmail.com>
2025-06-01 16:56:00 +0300

    Update Group.h

--- CPP/Class Group/Group/Group.h
+++ CPP/Class Group/Group/Group.h
@@ -1,8 +1,32 @@
 #pragma once
+#include <iostream> 
 
 class Student {
 public:
     char name[50];
     char university[50];
     int age;
+};
+
+class Group {
+private:
+	Student** _student;
+	char* _name;
+	int _size;
+public:
+	Group(char* name = nullptr) {}
+
+	Group(Group& group) {}
+
+	Student** GetStudent() {}
+
+	Student** SetStudent(Student& student) {}
+
+	char* SetName(char* name) {}
+
+	Student** DeleteStudent(int index) {}
+
+	void Studentinfo(int index) {}
+
+	~Group() {}
 };
