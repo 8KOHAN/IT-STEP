9f0567dcf91f3c9d8b40c495139dad4808c9ceda
Alexey Kohan <kohanzom@gmail.com>
2025-06-02 21:20:14 +0300

    Update main.cpp

--- CPP/Class Group/main.cpp
+++ CPP/Class Group/main.cpp
@@ -3,5 +3,50 @@
 
 int main()
 {
+    Group group((char*)"П42");
+    int choice;
+    do {
+        std::cout << "1. Add student\n";
+        std::cout << "2. Delete student\n";
+        std::cout << "3. Show student information\n";
+        std::cout << "0. Exit\n";
+        std::cout << "Your choice: ";
 
+        std::cin >> choice;
+        switch (choice) {
+        case 1: {
+            Student s;
+            std::cout << "Enter name: ";
+            std::cin >> s.name;
+            std::cout << "Age: ";
+            std::cin >> s.age;
+            std::cout << "University: ";
+            std::cin >> s.university;
+            group.SetStudent(s);
+            break;
+        }
+
+        case 2: {
+            int index;
+            std::cout << "Enter student index to delete: ";
+            std::cin >> index;
+            group.DeleteStudent(index);
+            break;
+        }
+
+        case 3: {
+            for (int i = 0; i < group.GetSize(); ++i) {
+                group.Studentinfo(i);
+            }
+            break;
+        }
+
+        case 0:
+            std::cout << "Exiting\n";
+            break;
+
+        default:
+            std::cout << "Invalid choice!\n";
+        }
+    } while (choice != 0);
 }
