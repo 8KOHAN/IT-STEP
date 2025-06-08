6a7b6f307cedca8cd5f8b37308e14282ff77e0b9
Alexey Kohan <kohanzom@gmail.com>
2025-06-08 09:45:43 +0300

    Create Overcoat.h

---
+++ CPP/Class Overcoat and Flat/Overcoat/Overcoat.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+class Overcoat {
+private:
+    std::string type;
+    double price;
+
+public:
+    Overcoat(std::string t = "", double p = 0.0);
+
+    bool operator==(const Overcoat& other) const;
+
+    Overcoat& operator=(const Overcoat& other);
+
+    bool operator>(const Overcoat& other) const;
+
+    void display() const;
+};
