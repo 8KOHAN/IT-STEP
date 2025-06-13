711a52f51aee1050836350c491d267e01906520a
Alexey Kohan <kohanzom@gmail.com>
2025-06-13 17:47:01 +0300

    Implementation of constructors and destructor

--- CPP/Class String/String/String.cpp
+++ CPP/Class String/String/String.cpp
@@ -1,24 +1,34 @@
-explicit String::String(const char* str) {
+#include "String.h"
 
+explicit String::String(const char* str) : length_(strlen(str)) {
+	str_ = new char[length_ + 1];
+	strcpy_s(str_, length_ + 1, str);
 }
-String::String(const String& str) {
-
+String::String(const String& str) : length_(str.length_) {
+	str_ = new char[length_ + 1];
+	strcpy_s(str_, length_ + 1, str.str_);
 }
-String::String(String&& str) noexcept {
-
+String::String(String&& str) noexcept : str_(str.str_), length_(str.length_) {
+	delete[] str.str_;
+	str.length_ = 0;
 }
-
-String& String::operator = (String& str) {
-
+String& String::operator = (const String& str) {
+	length_ = str.length_;
+	str_ = new char[length_ + 1];
+	strcpy_s(str_, length_ + 1, str.str_);
 }
 String& String::operator = (String&& str) noexcept {
-
+	str_ = str.str_;
+	length_ = str.length_;
+	delete[] str.str_;
+	str.length_ = 0;
 }
-
 String::~String() {
 	delete str_;
 }
-
+size_t String::length() const {
+	return length_;
+}
 char* String::begin() const {
 
 }
@@ -37,7 +47,6 @@ char String::back() const {
 void String::clear() {
 
 }
-
 void String::insert(char arr[]) {
 
 }
@@ -50,7 +59,6 @@ void String::replace(char arr[]) {
 bool String::find(char arr[]) {
 
 }
-
 bool String::compare(String str) {
 
 }
