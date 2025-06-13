a8d522300592a6ef834c9655da3c2419b604b97b
Alexey Kohan <kohanzom@gmail.com>
2025-06-13 18:42:46 +0300

    Update String.h

--- CPP/Class String/String/String.h
+++ CPP/Class String/String/String.h
@@ -4,43 +4,31 @@
 
 class String
 {
-private:
-	char* str_;
-	int size_;
 public:
 	explicit String(const char* str = "");
-
 	String(const String& str);
 	String(String&& str) noexcept;
-	String& operator = (String& str);
+	String& operator = (const String& str);
 	String& operator = (String&& str) noexcept;
 	~String();
-
+	size_t length() const;
 	char* begin() const;
 	char* end() const;
 	bool empty() const;
 	char front() const;
 	char back() const;
 	void clear();
-
-	void insert(char arr[]);
-	void erase(char arr[]);
-	void replace(char arr[]);
-	bool find(char arr[]);
-
+	size_t find(char arr[], int size);
 	bool compare(String str);
 	bool compare(char str[]);
-
+	bool operator==(const String& other) const;
+	bool operator==(const char* cstr) const;
+	String operator+(const String& str);
+	char& operator[](size_t index);
+	const char& operator[](size_t index) const;
+	friend std::istream& operator>>(std::istream& is, String& str);
+	friend std::ostream& operator<<(std::ostream& os, const String& str);
+private:
+	char* str_;
+	size_t length_;
 };
-
-
-
-//begin / end - получить итератор для строки
-//empty - проверка на пустоту
-//clear - очистка строки
-//front / back - получить первый / последний символ
-//insert - вставить подстроку
-//erase - удалить подстроку
-//replace - изменить подстроку
-//find - найти подстроку
-//compare - сравнить две строки
