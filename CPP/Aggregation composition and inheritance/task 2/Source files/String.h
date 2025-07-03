8b18339c30089fc6743cb89c391dff02d325a076
Alexey Kohan <kohanzom@gmail.com>
2025-07-03 13:32:14 +0300

    Create String.h

---
+++ CPP/Aggregation composition and inheritance/task 2/Source files/String.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+
+class String
+{
+public:
+	explicit String(const char* str = "");
+	String(const String& str);
+	String(String&& str) noexcept;
+	String& operator = (const String& str);
+	String& operator = (String&& str) noexcept;
+	~String();
+
+	size_t length() const;
+	char* begin() const;
+	char* end() const;
+	bool empty() const;
+	char front() const;
+	char back() const;
+	void clear();
+	size_t find(char arr[], int size);
+	bool compare(String str) const;
+	bool compare(char str[]) const;
+	bool operator==(const String& other) const;
+	bool operator==(const char* cstr) const;
+	bool operator!=(const String& other) const;
+	bool operator!=(const char* cstr) const;
+	String operator+(const String& str) const;
+	String& operator+=(const String& str);
+	char& operator[](size_t index);
+	const char& operator[](size_t index) const;
+
+	friend std::istream& operator>>(std::istream& is, String& str);
+	friend std::ostream& operator<<(std::ostream& os, const String& str);
+	friend bool operator==(const char* cstr, const String& str);
+	friend bool operator!=(const char* cstr, const String& str);
+private:
+	char* str_;
+	size_t length_;
+};
