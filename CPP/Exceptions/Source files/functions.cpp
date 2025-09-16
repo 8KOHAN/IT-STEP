ebcbd8bafc639edec412aa58caf59d556ab3cc64
Geralt Roger <kohanzom@gmail.com>
2025-09-16 11:32:23 +0300

    Update functions.cpp

--- CPP/Exceptions/Source files/functions.cpp
+++ CPP/Exceptions/Source files/functions.cpp
@@ -2,7 +2,7 @@
 
 int toInt(const std::string& s) {
     size_t pos;
-    int value = std::stoi(s, &pos);
+    int value = std::stoi(s, &pos); // std::stoi (String to Integer) — це стандартна функція з <string>, яка конвертує std::string в ціле число типу int.
 
     if (pos != s.size()) {
         throw std::invalid_argument("Invalid string: there are extra characters");
