404b3a9bf6d009da16c89240682d488650f453c5
Geralt Roger <kohanzom@gmail.com>
2025-09-17 10:03:46 +0300

    Update Group.cpp

--- CPP/Class Group/Group/Group.cpp
+++ CPP/Class Group/Group/Group.cpp
@@ -1,126 +1,34 @@
-#include <iostream>
 #include "Group.h"
+#include <iostream>
 
-Group::Group(char* name) {
-	_student = new Student* [1];
-	if (name){
-			_name = new char[strlen(name) + 1];
-			strcpy_s(_name, strlen(name) + 1, name);
-		}
-	else _name = nullptr;
-	_size = 0;
+Group::Group(const std::string& name) : name(name) {}
+
+std::string Group::GetName() const { return name; }
+void Group::SetName(const std::string& name) { this->name = name; }
+
+void Group::AddStudent(const Student& student) {
+    students.push_back(student);
 }
 
-Group::Group(const Group& group) {
-	if (group.GetSize() == 0) {
-			std::cout << "empty list Student" << std::endl;
-			_student = new Student* [1];
-		}
-	else {
-		_student = new Student* [group.GetSize()];
-		for (int i = 0; i < group.GetSize(); ++i)
-			_student[i] = new Student(*group.GetStudent()[i]);
-	}
-
-	if (group.GetName()) {
-			_name = new char[strlen(group.GetName()) + 1];
-			strcpy_s(_name, strlen(group.GetName()) + 1, group.GetName());
-		}
-	else {
-			std::cout << "name == nullptr" << std::endl;
-			_name = nullptr;
-		}
-
-	_size = group.GetSize();
+void Group::DeleteStudent(int index) {
+    if (index - 1 < 0 || index - 1 >= students.size()) {
+        std::cout << "Invalid index!" << std::endl;
+        return;
+    }
+    students.erase(students.begin() + (index - 1));
 }
 
-Student** Group::GetStudent() const {
-	if (_student) return _student;
-
-	std::cout << "Student is not initialized (_student == nullptr)" << std::endl;
-	return nullptr;
-}
-
-char* Group::GetName() const {
-	if (_name) return _name;
-
-	std::cout << "name is not initialized (_name == nullptr)" << std::endl;
-	return nullptr;
+void Group::ShowStudents() const {
+    if (students.empty()) {
+        std::cout << "No students in the group." << std::endl;
+        return;
+    }
+    for (size_t i = 0; i < students.size(); ++i) {
+        std::cout << i + 1 << ". ";
+        students[i].PrintInfo();
+    }
 }
 
 int Group::GetSize() const {
-	return _size;
-}
-
-Student** Group::SetStudent(Student& student) {
-	Student** temp = new Student* [_size + 1];
-	for (int i = 0; i < _size; ++i) {
-		temp[i] = _student[i];
-	}
-	temp[_size++] = new Student(student);
-
-	delete[] _student;
-
-	_student = temp;
-	return _student;
-}
-
-char* Group::SetName(char* name) {
-	if (name) {
-		delete[] _name;
-		_name = new char[strlen(name) + 1];
-		strcpy_s(_name, strlen(name) + 1, name);
-		return _name;
-	}
-	std::cout << "name has not been changed (parameter == nullptr)" << std::endl;
-	return _name;
-}
-
-Student** Group::DeleteStudent(int index) {
-	if (index < 0) {
-		std::cout << "_student is not changed index cannot be less than zero" << std::endl;
-		return _student;
-	}
-	if (index >= _size) {
-		std::cout << "_student is not changed index cannot be greater than size" << std::endl;
-		return _student;
-	}
-
-
-	Student** temp = new Student* [_size - 1];
-
-	int k = 0;
-	for (int i = 0; i < _size; ++i) {
-		if (i == index) continue;
-		temp[k++] = _student[i];
-	}
-	delete _student[index];
-	delete[] _student;
-
-	--_size;
-	_student = temp;
-	return _student;
-}
-
-void Group::Studentinfo(int index) {
-	if (index < 0) {
-		std::cout << "index cannot be less than zero" << std::endl;
-		return;
-	}
-	if (index >= _size) {
-		std::cout << "index cannot be greater than size" << std::endl;
-		return;
-	}
-
-	std::cout << "name: " << _student[index]->name << "age: " << _student[index]->age << "university: " <<  _student[index]->university << std::endl;
-}
-
-Group::~Group() {
-	if (_size > 0) {
-		for (int i = 0; i < _size; ++i) {
-			delete _student[i];
-		}
-	}
-	delete[] _student;
-	delete[] _name;
+    return students.size();
 }
