c636566fd12a1a531e51a21a71a4ef315f304dac
Geralt Roger <kohanzom@gmail.com>
2025-09-19 13:19:41 +0300

    Update purse.cpp

--- CPP/personal finance management system/Source Files/purse.cpp
+++ CPP/personal finance management system/Source Files/purse.cpp
@@ -1,1 +1,15 @@
-//coming
+#include "purse.h"
+
+Purse::Purse(std::string name): name_(name) { }
+
+const std::string Purse::name() const{
+	return name_;
+}
+
+const size_t Purse::AmountCardsD() const{
+	return numberCardsD_;
+}
+
+const size_t Purse::AmountCardsC() const{
+	return numberCardsC_;
+}
