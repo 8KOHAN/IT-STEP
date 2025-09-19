2239cfa0f4a27ae16b23cd677dafb7a6e5da99cc
Geralt Roger <kohanzom@gmail.com>
2025-09-19 13:20:06 +0300

    Update CardDebit.cpp

--- CPP/personal finance management system/Source Files/CardDebit.cpp
+++ CPP/personal finance management system/Source Files/CardDebit.cpp
@@ -1,1 +1,17 @@
-//coming
+#include "CardDebit.h"
+
+CardDebit::CardDebit(std::string name) : name_(name), AmountMoney_(0.) { }
+
+const double CardDebit::quantity() const {
+	return AmountMoney_;
+}
+
+void CardDebit::replenishment(double sum) {
+	if (sum <= 0) return;
+	AmountMoney_ += sum;
+}
+
+void CardDebit::spend(double sum) {
+	if ((AmountMoney_ - sum) < 0) return;
+	AmountMoney_ -= sum;
+}
