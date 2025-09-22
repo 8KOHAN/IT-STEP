542e799cd7c331e1d7a0d2ed357994cfe6d3403d
Geralt Roger <kohanzom@gmail.com>
2025-09-22 09:51:56 +0300

    Update purse.cpp

--- CPP/personal finance management system/Source Files/purse.cpp
+++ CPP/personal finance management system/Source Files/purse.cpp
@@ -1,15 +1,29 @@
 #include "purse.h"
 
-Purse::Purse(std::string name): name_(name) { }
+Purse::Purse(const std::string name): name_(name), amountCardsC_(0), amountCardsD_(0) { }
 
 const std::string Purse::name() const{
 	return name_;
 }
 
-const size_t Purse::AmountCardsD() const{
-	return numberCardsD_;
+const size_t Purse::amountCardsD() const{
+	return amountCardsD_;
 }
 
-const size_t Purse::AmountCardsC() const{
-	return numberCardsC_;
+const size_t Purse::amountCardsC() const{
+	return amountCardsC_;
+}
+
+void Purse::setName(const std::string newName) {
+	name_ = newName;
+}
+
+void Purse::addCardD(const std::string name) {
+	cardsD_.push_back(name);
+	++amountCardsD_;
+}
+
+void Purse::addCardC(const std::string name) {
+	cardsC_.push_back(name);
+	++amountCardsC_;
 }
