adc6d13706e352388e10a6061076cfd39c62d307
Geralt Roger <kohanzom@gmail.com>
2025-09-26 11:46:49 +0300

    Update main_functions.h

--- CPP/personal finance management system/Header Files/main_functions.h
+++ CPP/personal finance management system/Header Files/main_functions.h
@@ -2,8 +2,22 @@
 #include <iostream>
 #include "purse.h"
 
-int inputNum(int maxNum);
+int inputNum(int minNum, int maxNum);
+
+void printPurses(const std::vector<Purse>& purses);
 
 void createPurse(std::vector<Purse>& purses);
 
 int choicePurse(const std::vector<Purse>& purses);
+
+void setNamePurse(std::vector<Purse>& purses);
+
+bool checkExit(int day);
+
+void createCardD(std::vector<Purse>& purses, int numPurse);
+
+void createCardC(std::vector<Purse>& purses, int numPurse);
+
+void choiceCardD(const std::vector<Purse>& purses, int numPurse);
+
+void choiceCardC(const std::vector<Purse>& purses, int numPurse);
