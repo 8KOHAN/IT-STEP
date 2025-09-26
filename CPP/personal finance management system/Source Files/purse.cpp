c8b580bcd78928801f4c693f90a213bc7c2e7cd3
Geralt Roger <kohanzom@gmail.com>
2025-09-26 11:45:26 +0300

    Update purse.cpp

--- CPP/personal finance management system/Source Files/purse.cpp
+++ CPP/personal finance management system/Source Files/purse.cpp
@@ -1,29 +1,39 @@
 #include "purse.h"
 
-Purse::Purse(const std::string& name): name_(name), amountCardsC_(0), amountCardsD_(0) { }
+Purse::Purse(const std::string& name) noexcept : name_(name), amountCardsC_(0), amountCardsD_(0) { }
 
-const std::string Purse::name() const{
+const std::string Purse::name() const noexcept {
 	return name_;
 }
 
-const size_t Purse::amountCardsD() const{
+const size_t Purse::amountCardsD() const noexcept {
 	return amountCardsD_;
 }
 
-const size_t Purse::amountCardsC() const{
+const size_t Purse::amountCardsC() const noexcept {
 	return amountCardsC_;
 }
 
-void Purse::setName(const std::string& newName) {
+void Purse::setName(const std::string& newName) noexcept {
 	name_ = newName;
 }
 
-void Purse::addCardD(const std::string& name) {
+void Purse::addCardD(const std::string& name) noexcept {
 	cardsD_.push_back(name);
 	++amountCardsD_;
 }
 
-void Purse::addCardC(const std::string& name) {
+void Purse::addCardC(const std::string& name) noexcept {
 	cardsC_.push_back(name);
 	++amountCardsC_;
+}
+
+void Purse::printCardsD() const noexcept {
+	for (int i = 1; i < cardsD_.size(); ++i)
+		std::cout  << "карта номер " << i << " - " << cardsD_[i].name() << std::endl;
+}
+
+void Purse::printCardsC() const noexcept {
+	for (int i = 1; i < cardsC_.size(); ++i)
+		std::cout << "карта номер " << i << " - " << cardsC_[i].name() << std::endl;
 }
