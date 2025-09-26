0efd88cfdd131206411e9473b0f33ebeb7537e0d
Geralt Roger <kohanzom@gmail.com>
2025-09-26 11:45:50 +0300

    Update CardCredit.h

--- CPP/personal finance management system/Header Files/CardCredit.h
+++ CPP/personal finance management system/Header Files/CardCredit.h
@@ -4,12 +4,12 @@
 class CardCredit : public CardDebit
 {
 public:
-	CardCredit(const std::string& name);
+	CardCredit(const std::string& name) noexcept;
 
-	const double checkCredit() const;
-	void spend(const double sum);
-	void takeСredit(const double sum);
-	void returnCredit(const double sum);
+	const double checkCredit() const noexcept;
+	void spend(const double sum) noexcept override;
+	void takeСredit(const double sum) noexcept;
+	void returnCredit(const double sum) noexcept;
 private:
 	double duty_;
 };
