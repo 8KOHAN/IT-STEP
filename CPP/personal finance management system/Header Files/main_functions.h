473d106040fbc6d3c7595bfa4cba7ff51662d544
Geralt Roger <kohanzom@gmail.com>
2025-09-27 13:54:39 +0300

    Update main_functions.h

--- CPP/personal finance management system/Header Files/main_functions.h
+++ CPP/personal finance management system/Header Files/main_functions.h
@@ -2,13 +2,15 @@
 #include <iostream>
 #include "purse.h"
 
-int inputNum(int minNum, int maxNum);
+const int inputNum(const int minNum, const int maxNum);
+
+const double inputDouble();
 
 void printPurses(const std::vector<Purse>& purses);
 
 void createPurse(std::vector<Purse>& purses);
 
-int choicePurse(const std::vector<Purse>& purses);
+const int choicePurse(const std::vector<Purse>& purses);
 
 void setNamePurse(std::vector<Purse>& purses);
 
@@ -18,6 +20,12 @@ void createCardD(std::vector<Purse>& purses, int numPurse);
 
 void createCardC(std::vector<Purse>& purses, int numPurse);
 
-void choiceCardD(const std::vector<Purse>& purses, int numPurse);
+const int choiceCardD(const std::vector<Purse>& purses, int numPurse);
 
-void choiceCardC(const std::vector<Purse>& purses, int numPurse);
+const int choiceCardC(const std::vector<Purse>& purses, int numPurse);
+
+const double amountMoneyCard(const std::vector<Purse>& purses, int numPurse, char Debit_or_Credit, int numCard);
+
+void replenishment(const std::vector<Purse>& purses, int numPurse, char Debit_or_Credit, int numCard);
+
+void takeСredit(std::vector<Purse>& purses, int numPurse, int numCard);
