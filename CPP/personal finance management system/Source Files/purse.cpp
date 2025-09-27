810b3b08f94d56168622769f360b146442f4b8c9
Geralt Roger <kohanzom@gmail.com>
2025-09-27 13:57:36 +0300

    Update purse.cpp

--- CPP/personal finance management system/Source Files/purse.cpp
+++ CPP/personal finance management system/Source Files/purse.cpp
@@ -14,6 +14,26 @@ const size_t Purse::amountCardsC() const noexcept {
 	return amountCardsC_;
 }
 
+const double Purse::amountMoneyCardD(int index) const {
+	return cardsD_[index].quantity();
+}
+
+const double Purse::amountMoneyCardC(int index) const {
+	return cardsC_[index].quantity();
+}
+
+void Purse::replenishmentCardD(double sum, int index) {
+	cardsD_[index].replenishment(sum);
+}
+
+void Purse::replenishmentCardC(double sum, int index) {
+	cardsC_[index].replenishment(sum);
+}
+
+void Purse::takeСredit(double sum, int index){
+	cardsC_[index].takeСredit(sum);
+}
+
 void Purse::setName(const std::string& newName) noexcept {
 	name_ = newName;
 }
