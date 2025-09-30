ccf860b56afd52d4de1d38dac974441323d6c9f8
Geralt Roger <kohanzom@gmail.com>
2025-09-30 13:26:46 +0300

    Update CardDebit.cpp

--- CPP/personal finance management system/Source Files/CardDebit.cpp
+++ CPP/personal finance management system/Source Files/CardDebit.cpp
@@ -1,9 +1,13 @@
 #include "CardDebit.h"
 
-CardDebit::CardDebit(const std::string& name) noexcept : name_(name), amountMoney_(0.) { }
+CardDebit::CardDebit(const std::string& name, char currency) noexcept : name_(name), amountMoney_(0.), currency_(currency){ }
 
 const std::string CardDebit::name() const noexcept {
 	return name_;
+}
+
+const char CardDebit::currency() const noexcept {
+	return currency_;
 }
 
 void CardDebit::setName(const std::string& newName) noexcept {
