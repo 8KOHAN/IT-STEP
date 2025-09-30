06d55bfbf6d792fb41a057aeed5ef3c31dfa8bba
Geralt Roger <kohanzom@gmail.com>
2025-09-30 13:25:19 +0300

    Update Shop.h

--- CPP/personal finance management system/Header Files/Shop.h
+++ CPP/personal finance management system/Header Files/Shop.h
@@ -2,18 +2,35 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "purse.h"
+#include "functions.h"
+#include "Products.h"
 
 class Shop
 {
 public:
-	Shop(std::string fileName);
+	Shop(std::string fileName, int& day);
 
-	void buy(int choice);
+	void printProducts(char currency) const;
+
+	void buy(std::vector<Purse>& purses, int numPurse, char Debit_or_Credit, int numCard);
+
+	void statisticsDay();
+	void statisticsWeek();
+	void statisticsMonth();
+
+	void topSpendingThisWeek();
+	void topSpendingThisMonth();
+
+private:
+	std::string spendingHistory_ = "";
+	std::string fileName_;
+	AmountProducts amountProducts_;
+	InStockProducts inStockProducts_;
+	double amountExpenses_ = 0;
+	int& day_;
 
 	void saveToFile();
 
-private:
-	std::string spendingHistory_;
-	std::string fileName_;
-	// написать поля для каждого товара
 };
+
