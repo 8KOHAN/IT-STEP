011acb023d9e20e8c5d807a49a03e5a8cec866b9
Geralt Roger <kohanzom@gmail.com>
2025-10-01 19:18:54 +0300

    Update CardCredit.cpp

--- CPP/personal finance management system/Source Files/CardCredit.cpp
+++ CPP/personal finance management system/Source Files/CardCredit.cpp
@@ -1,17 +1,18 @@
 #include "CardCredit.h"
 
-CardCredit::CardCredit(const std::string& name) noexcept : CardDebit(name), duty_(0.) {}
+CardCredit::CardCredit(const std::string& name, char currency) noexcept : CardDebit(name, currency), duty_(0.) {}
 
 const double CardCredit::checkCredit() const noexcept { return duty_; }
 
 void CardCredit::spend(const double sum) noexcept {
 	if ((amountMoney_ - sum) < 0) {
 		std::cout << "вам не хватает: " << (amountMoney_ - sum) / -1 <<std::endl;
-		std::cout << "хотите взять кредит?\nyes/not";
+		std::cout << "хотите взять кредит?\nyes/not\n";
 		std::string answer;
+		std::cout << "ваш выбор: ";
 		do {
-		std::cin >> answer;
-		} while (answer != "yes" || answer != "not");
+			std::cin >> answer;
+		} while (!(answer == "yes" && answer == "not"));
 
 		if (answer == "yes") {
 			std::cout << "введите суму кредита - ";
@@ -35,6 +36,10 @@ void CardCredit::takeСredit(const double sum) noexcept {
 void CardCredit::returnCredit(const double sum) noexcept {
 	if ((amountMoney_ - sum) < 0) {
 		std::cout << "не удалось погасить кредит" << std::endl;
+		return;
+	}
+	if ((duty_ - sum) < 0) {
+		std::cout << "введина слишком большая сума, ван нужно вернуть " << duty_ << std::endl;
 		return;
 	}
 	amountMoney_ -= sum;
