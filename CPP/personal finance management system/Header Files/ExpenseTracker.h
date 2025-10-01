c46abfb164af0f39d59cf324d5771955d3063975
Geralt Roger <kohanzom@gmail.com>
2025-10-01 19:13:04 +0300

    Update ExpenseTracker.h

--- CPP/personal finance management system/Header Files/ExpenseTracker.h
+++ CPP/personal finance management system/Header Files/ExpenseTracker.h
@@ -1,32 +1,27 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
-
-class Expense {
-public:
-    Expense(std::string message, double sum, char currency);
-    void print();
-private:
-    std::string message_;
-    double sum_;
-    char currency_;
-};
+#include <algorithm>
+#include "Expense.h"
 
 class ExpenseTracker {
 public:
-    ExpenseTracker();
+    ExpenseTracker(std::string fileName) noexcept;
 
-    void nextDay();
+    void nextDay() noexcept;
 
-    void addExpense(double sum, char currency, std::string message = "");
+    void addExpense(double sum, char currency, std::string message = "") noexcept;
 
-    void expensesForDay();
-    void expensesForWeek();
-    void expensesForMonth();
+    void expensesForDay() noexcept;
+    void expensesForWeek() noexcept;
+    void expensesForMonth() noexcept;
+
+    void topSpendingThisWeek() noexcept;
+    void topSpendingThisMonth() noexcept;
 private:
     std::vector<std::vector<Expense>> expenses;
+    std::string fileName_;
     int maxSize = 31;
-    int index = 0;
 };
-
