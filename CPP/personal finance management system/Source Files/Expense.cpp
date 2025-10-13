4a48d852b94e6b50f5afb8b91a69cd1c2158fde7
Geralt Roger <kohanzom@gmail.com>
2025-10-13 11:17:12 +0300

    Update Expense.cpp

--- CPP/personal finance management system/Source Files/Expense.cpp
+++ CPP/personal finance management system/Source Files/Expense.cpp
@@ -2,15 +2,15 @@
 
 Expense::Expense(std::string message, double sum, char currency) noexcept : message_(message), sum_(sum), currency_(currency) {}
 
-void Expense::print() noexcept {
-    std::cout << message_ << " за : " << sum_ << currency_ << std::endl;
+void Expense::print() const noexcept {
+    std::cout << message_ << " for : " << sum_ << currency_ << std::endl;
 }
 
-void Expense::writeToFile(std::ofstream& file) noexcept {
+void Expense::writeToFile(std::ofstream& file) const noexcept {
     if (!file.is_open()) {
-        std::cout << "Файл не открыт для записи!" << std::endl;
+        std::cout << "File not open for writing!" << std::endl;
     }
-    file << message_ << " за: " << sum_ << " " << currency_ << std::endl;
+    file << message_ << " for: " << sum_ << " " << currency_ << std::endl;
 }
 
 const double Expense::sum() const noexcept {
