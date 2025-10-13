57af985faa9960733ea14c0f44510ad4a902ce83
Geralt Roger <kohanzom@gmail.com>
2025-10-13 12:00:31 +0300

    Update task 1.cpp

--- CPP/Working-with-a-file-system/task 1.cpp
+++ CPP/Working-with-a-file-system/task 1.cpp
@@ -10,7 +10,7 @@ int main() {
     fopen_s(&file2, "file2.txt", "r");
 
     if (file1 == NULL || file2 == NULL) {
-        std::cout << stderr << " Не вдалося відкрити один або обидва файли.\n";
+        std::cout << stderr << " One or both files could not be opened." << std::endl;
         return 1;
     }
 
@@ -24,7 +24,7 @@ int main() {
         line2[strcspn(line2, "\n")] = '\0';
 
         if (strcmp(line1, line2) != 0) {
-            printf("Рядки не збігаються (рядок %d):\n", lineNumber);
+            printf("The strings do not match (string %d):\n", lineNumber);
             printf("file1.txt: %s\n", line1);
             printf("file2.txt: %s\n", line2);
             allMatch = 0;
@@ -34,16 +34,16 @@ int main() {
     }
 
     if (fgets(line1, MAX_LINE_LENGTH, file1)) {
-        printf("file1.txt має додатковий рядок (рядок %d): %s", lineNumber, line1);
+        printf("file1.txt has an additional line (line %d): %s", lineNumber, line1);
         allMatch = 0;
     }
     else if (fgets(line2, MAX_LINE_LENGTH, file2)) {
-        printf("file2.txt має додатковий рядок (рядок %d): %s", lineNumber, line2);
+        printf("file2.txt has an additional line (line %d): %s", lineNumber, line2);
         allMatch = 0;
     }
 
     if (allMatch) {
-        printf("Усі рядки збігаються.\n");
+        printf("All lines match.\n");
     }
 
     fclose(file1);
