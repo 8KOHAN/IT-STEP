d0559c6718d3d54c1d510d3ce9949349b7c9869a
Geralt Roger <kohanzom@gmail.com>
2025-10-14 10:04:48 +0300

    Update main.cs

--- .NET and C#/Introduction to .NET and C#/Temperature conversion/main.cs
+++ .NET and C#/Introduction to .NET and C#/Temperature conversion/main.cs
@@ -2,30 +2,26 @@ namespace IT_STEP
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.InputEncoding = System.Text.Encoding.UTF8;
-
-
-            Console.Write("Введіть температуру: ");
+            Console.Write("Enter the temperature: ");
             double temp = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Оберіть напрямок конвертації (1 - F→C, 2 - C→F): ");
+            Console.Write("Choose the conversion direction (1 - F→C, 2 - C→F): ");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             if (choice == 1)
             {
                 double celsius = (temp - 32) * 5 / 9;
-                Console.WriteLine($"Температура у Цельсіях: {celsius:F2}°C");
+                Console.WriteLine($"Temperature in Celsius: {celsius:F2}°C");
             }
             else if (choice == 2)
             {
                 double fahrenheit = temp * 9 / 5 + 32;
-                Console.WriteLine($"Температура у Фаренгейтах: {fahrenheit:F2}°F");
+                Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit:F2}°F");
             }
             else 
-                Console.WriteLine("Невірний вибір.");
+                Console.WriteLine("Wrong choice");
         }
     }
 }
