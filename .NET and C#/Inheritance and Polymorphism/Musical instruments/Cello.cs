f617f16ae567b5da651fc5cd00acb7485c08dafe
Geralt Roger <kohanzom@gmail.com>
2025-10-29 15:17:40 +0200

    Create Cello.cs

---
+++ .NET and C#/Inheritance and Polymorphism/Musical instruments/Cello.cs
@@ -0,0 +1,19 @@
+namespace IT_STEP
+{
+    public class Cello : MusicalInstrument
+    {
+        public Cello() : base("Cello", "A bowed string instrument with a warm, rich tone") { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("The cello sounds deep and mellow 🎵");
+            Console.Beep(300, 400);
+            Console.Beep(400, 400);
+        }
+
+        public override void History()
+        {
+            Console.WriteLine("The cello appeared in the 16th century in Italy and became a key orchestral instrument.");
+        }
+    }
+}
