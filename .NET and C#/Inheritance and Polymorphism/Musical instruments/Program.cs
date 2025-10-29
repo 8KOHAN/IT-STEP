c18c33cdbd2418cfa9f75889418ea7f46c58d679
Geralt Roger <kohanzom@gmail.com>
2025-10-29 15:11:52 +0200

    Create Program.cs

---
+++ .NET and C#/Inheritance and Polymorphism/Musical instruments/Program.cs
@@ -0,0 +1,29 @@
+using MusicalInstruments;
+
+namespace IT_STEP
+{
+    class Program
+    {
+        static void Main()
+        {
+            MusicalInstrument[] instruments =
+            {
+                new Violin(),
+                new Trombone(),
+                new Ukulele(),
+                new Cello()
+            };
+
+            foreach (var instrument in instruments)
+            {
+                Console.WriteLine("------------------------------");
+                instrument.Show();
+                instrument.Desc();
+                instrument.History();
+                instrument.Sound();
+            }
+
+            Console.WriteLine("\nAll instruments demonstrated!");
+        }
+    }
+}
