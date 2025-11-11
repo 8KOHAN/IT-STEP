f5da7f4bfcf43a5355958304a9b6cbfcd0050b0d
Geralt Roger <kohanzom@gmail.com>
2025-11-11 09:45:19 +0200

    Create IRemoteControl.cs

---
+++ .NET and C#/Interfaces/Remote Control/IRemoteControl.cs
@@ -0,0 +1,9 @@
+namespace IT_STEP
+{
+    public interface IRemoteControl
+    {
+        public void TurnOn();
+        public void TurnOff();
+        public void SetChannel(int channel);
+    }
+}
