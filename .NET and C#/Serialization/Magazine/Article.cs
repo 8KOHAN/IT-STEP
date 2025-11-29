13b7d22a3adbeaad51f69e81f9c53bf9c72ce3cc
Geralt Roger <kohanzom@gmail.com>
2025-11-29 09:22:28 +0200

    Add Article class

---
+++ .NET and C#/Serialization/Magazine/Article.cs
@@ -0,0 +1,9 @@
+namespace IT_STEP
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public int CharacterCount { get; set; }
+        public string Preview { get; set; }
+    }
+}
