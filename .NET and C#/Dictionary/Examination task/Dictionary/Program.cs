d773b928854ef9c94e1d6a972ca09339f7683137
Geralt Roger <kohanzom@gmail.com>
2025-12-03 12:34:56 +0200

    Add initial Program.cs

---
+++ .NET and C#/Dictionary/Examination task/Dictionary/Program.cs
@@ -0,0 +1,11 @@
+namespace IT_STEP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var menu = new MainMenu();
+            menu.Show();
+        }
+    }
+}
