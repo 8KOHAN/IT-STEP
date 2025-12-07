00b6ab4390e52d80bd75cacda2658b1386882d4c
Geralt Roger <kohanzom@gmail.com>
2025-12-07 15:38:41 +0200

    Create Result.cs

---
+++ .NET and C#/Examination task/Quiz/Result.cs
@@ -0,0 +1,9 @@
+namespace IT_STEP
+{
+    public class Result
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
