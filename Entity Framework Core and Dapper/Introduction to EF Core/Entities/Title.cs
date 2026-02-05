97c3fe263a21f95288fd36a1d165d8af2b77d9e0
Geralt Roger <kohanzom@gmail.com>
2026-02-05 10:33:15 +0200

    Add Title entity with properties Id, Name, and Duration

---
+++ Entity Framework Core and Dapper/Introduction to EF Core/Entities/Title.cs
@@ -0,0 +1,9 @@
+namespace IT_STEP
+{
+    public class Title
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
