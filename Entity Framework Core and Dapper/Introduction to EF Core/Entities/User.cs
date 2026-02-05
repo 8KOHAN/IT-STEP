57353233ff98880d5eea66183e2aa45858006839
Geralt Roger <kohanzom@gmail.com>
2026-02-05 10:33:37 +0200

    Add User entity class for EF Core

---
+++ Entity Framework Core and Dapper/Introduction to EF Core/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace IT_STEP
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
