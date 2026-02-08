f3d6a46ffa99eda8e68f9c85360655ae0a973d9e
Geralt Roger <kohanzom@gmail.com>
2026-02-08 19:20:49 +0200

    Add Movie entity with properties and relationship

---
+++ Entity Framework Core and Dapper/Relationships between tables/Entities/Movie.cs
@@ -0,0 +1,14 @@
+namespace IT_STEP
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public int Year { get; set; }
+        public string Description { get; set; } = "";
+
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
