b19be788dbdfa3b8bbc56631f309706baf22f171
Geralt Roger <kohanzom@gmail.com>
2026-02-08 19:21:32 +0200

    Create MoviesContext for EF Core database setup
    Added MoviesContext class for database context management.

--- Entity Framework Core and Dapper/Relationships between tables/Data/MoviesContext.cs
+++ Entity Framework Core and Dapper/Relationships between tables/Data/MoviesContext.cs
@@ -1,1 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 
+namespace IT_STEP
+{
+    public class MoviesContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(
+                @"Server=ALEKSEY\IT_STEP;
+                  Database=MoviesDb;
+                  Trusted_Connection=True;
+                  TrustServerCertificate=True;");
+        }
+    }
+}
