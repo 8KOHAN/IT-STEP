3f9a7533ab36a60b1811e6a5450a261eda8572d8
Geralt Roger <kohanzom@gmail.com>
2026-02-19 12:40:10 +0200

    Create Dog.cs

---
+++ Entity Framework Core and Dapper/Dapper Learn/Entities/Dog.cs
@@ -0,0 +1,12 @@
+namespace IT_STEP
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public bool IsAdopted { get; set; } = false;
+    }
+
+}
