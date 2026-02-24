53dede4a61310f3c88a7ab6a46795c3c61d48545
Geralt Roger <kohanzom@gmail.com>
2026-02-24 14:58:41 +0200

    Add SessionInfo model for game session tracking

---
+++ Entity Framework Core and Dapper/Final task/BoardGamesAnalytics/Models/SessionInfo.cs
@@ -0,0 +1,8 @@
+namespace BoardGamesAnalytics.Models;
+public class SessionInfo
+{
+    public string GameTitle { get; set; }
+    public string MemberName { get; set; }
+    public DateTime Date { get; set; }
+    public int DurationMinutes { get; set; }
+}
