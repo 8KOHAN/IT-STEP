a9722b416cfd31dbaa94952f3438171e16bc4d4a
Geralt Roger <kohanzom@gmail.com>
2026-03-06 09:48:19 +0200

    Implement multi-threaded number generation and analysis
    This program generates random numbers and calculates their maximum, minimum, and average values using multiple threads. It uses a ManualResetEvent to signal when the numbers are ready for processing.

---
+++ System programming/Synchronization part 2/Program.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static List<int> numbers = new List<int>();
+    static ManualResetEvent dataReadyEvent = new ManualResetEvent(false);
+
+    static void Main(string[] args)
+    {
+        Thread generatorThread = new Thread(GenerateNumbers);
+        Thread maxThread = new Thread(FindMax);
+        Thread minThread = new Thread(FindMin);
+        Thread avgThread = new Thread(FindAverage);
+
+        maxThread.Start();
+        minThread.Start();
+        avgThread.Start();
+
+        generatorThread.Start();
+
+        generatorThread.Join();
+        maxThread.Join();
+        minThread.Join();
+        avgThread.Join();
+    }
+
+    static void GenerateNumbers()
+    {
+        Random rand = new Random();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            numbers.Add(rand.Next(0, 5001));
+        }
+
+        Console.WriteLine("Number generation finished.");
+
+        dataReadyEvent.Set();
+    }
+
+    static void FindMax()
+    {
+        dataReadyEvent.WaitOne();
+
+        int max = int.MinValue;
+
+        foreach (var n in numbers)
+        {
+            if (n > max)
+                max = n;
+        }
+
+        Console.WriteLine("Maximum value: " + max);
+    }
+
+    static void FindMin()
+    {
+        dataReadyEvent.WaitOne();
+
+        int min = int.MaxValue;
+
+        foreach (var n in numbers)
+        {
+            if (n < min)
+                min = n;
+        }
+
+        Console.WriteLine("Minimum value: " + min);
+    }
+
+    static void FindAverage()
+    {
+        dataReadyEvent.WaitOne();
+
+        double sum = 0;
+
+        foreach (var n in numbers)
+        {
+            sum += n;
+        }
+
+        double avg = sum / numbers.Count;
+
+        Console.WriteLine("Average value: " + avg);
+    }
+}
