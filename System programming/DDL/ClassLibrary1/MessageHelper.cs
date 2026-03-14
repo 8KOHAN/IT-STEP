786d89e61bc5f73ef169df6fc0ed5fd15edbcf1c
Geralt Roger <kohanzom@gmail.com>
2026-03-14 20:09:10 +0200

    Create MessageHelper.cs

---
+++ System programming/DDL/ClassLibrary1/MessageHelper.cs
@@ -0,0 +1,10 @@
+namespace ClassLibrary1
+{
+    public class MessageHelper
+    {
+        public static void ShowMessage()
+        {
+            Console.WriteLine("this message from DLL!");
+        }
+    }
+}
