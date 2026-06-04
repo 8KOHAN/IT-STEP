4342d7c6a13f811cc54409fa9f68437d4aa679ea
Geralt Roger <kohanzom@gmail.com>
2026-06-04 14:15:54 +0300

    Fix console.log formatting for even and short words

--- JavaScript/Promises part 2/task 2/script.js
+++ JavaScript/Promises part 2/task 2/script.js
@@ -21,7 +21,7 @@ function isShortWord(word) {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const evenNumbers = filterArray(numbers, isEven);
 
-console.log(evenNumbers); 
+console.log(evenNumbers);
 
 const words = ["cat", "elephant", "dog", "tree", "computer"];
 const shortWords = filterArray(words, isShortWord);
