274b40e009df636449e6f1fde3550f66c3e7422f
8KOHAN <kohanzom@gmail.com>
2026-06-18 15:48:08 +0300

    added foder and nav

--- React with TypeScript/clock website/src/Pages/_layout/Layout.tsx
+++ React with TypeScript/clock website/src/Pages/_layout/Layout.tsx
@@ -3,14 +3,46 @@ import "./ui/Layout.css"
 
 export default function Layout() {
     return <>
-        <nav>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/group">Contact</Link>
+
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link"><i className="bi bi-house"></i></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/group" className="nav-link"><i className="bi bi-cart"></i></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/no-page" className="nav-link"><i className="bi bi-sign-stop"></i></Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="button">Search</button>
+                    </form>
+                </div>
+            </div>
         </nav>
 
-        <Outlet/>
 
-        <footer>
+        <nav>
+            <Link to="/">Home</Link> |{" "}
+            <Link to="/group">Group</Link>
+        </nav>
+
+        <main>
+
+            <Outlet />
+        </main>
+
+        <footer className="border-top bg">
             &copy; IT STEP, 2026
         </footer>
     </>
--- React with TypeScript/clock website/src/Pages/home/home.tsx
+++ React with TypeScript/clock website/src/Pages/home/home.tsx
@@ -43,7 +43,7 @@ export default function Home() {
     }
 
     return (
-        <div>
+        <div className="home-wrapper">
             <Counter />
             <Counter />
 
